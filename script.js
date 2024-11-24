4a253691bd5b2b4ceffaf8dcf24740d2fbede043
Muhammad Tanveer <m.tanveercs@gmail.com>
2024-11-25 00:14:41 +0500

    skill section completed

--- script.js
+++ script.js
@@ -33,3 +33,29 @@ let changeText = () => {
 };
 changeText();
 setInterval(changeText, 3000);
+
+
+
+// circle skill //////////////
+debugger
+const circles =document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+  var dots = elem.getAttribute("data-dots");
+  var marked = elem.getAttribute("data-percent")
+  var percent = Math.floor(dots*marked/100)
+  var points = ''
+  var rotate = 360 / dots;
+
+
+  for (let i = 0; i < dots; i++) {
+    points += ` <div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`
+    
+  }
+  elem.innerHTML =points;
+
+  const pontsMarked =elem.querySelectorAll('.points');
+  for (let i = 0; i < percent; i++) {
+    pontsMarked[i].classList.add('marked')
+    
+  }
+})
